73f1b6b5513415629dd9e005a7c3136e8d372348
rpaquia <97699019+rpaquia@users.noreply.github.com>
2022-02-10 12:57:05 -0800

    Jared Edits

--- grade.sh
+++ grade.sh
@@ -1,6 +1,13 @@
-
+FILE=MarkdownParse.java
 git clone $1
 cp GradingTests.java markdown-parse/
+cp -r lib markdown-parse/
 cd markdown-parse
+
+if [ ! -f "$FILE"] ; then
+    echo "$FILE not found."
+    exit 1
+fi
+
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
